4efbf044806bfc9d707ea2590ff092d9292bd07d
ciberdavisxp <ciberdavisxp@gmail.com>
2024-12-04 22:46:57 -0600

    front-dc

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Grafos
+namespace Grafos
 {
     partial class MainForm
     {
@@ -33,22 +33,22 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.txtRecorrido = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnHonduras = new System.Windows.Forms.Button();
             this.btnElSalvador = new System.Windows.Forms.Button();
             this.btnGuatemala = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAnchura
             // 
             this.btnAnchura.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnAnchura.Location = new System.Drawing.Point(275, 21);
+            this.btnAnchura.Location = new System.Drawing.Point(140, 106);
             this.btnAnchura.Name = "btnAnchura";
             this.btnAnchura.Size = new System.Drawing.Size(97, 27);
             this.btnAnchura.TabIndex = 1;
@@ -58,7 +58,7 @@
             // 
             // btnProfundidad
             // 
-            this.btnProfundidad.Location = new System.Drawing.Point(275, 54);
+            this.btnProfundidad.Location = new System.Drawing.Point(140, 73);
             this.btnProfundidad.Name = "btnProfundidad";
             this.btnProfundidad.Size = new System.Drawing.Size(97, 27);
             this.btnProfundidad.TabIndex = 2;
@@ -70,39 +70,49 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 17;
-            this.listBox1.Location = new System.Drawing.Point(389, 21);
+            this.listBox1.Location = new System.Drawing.Point(241, 44);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(136, 55);
+            this.listBox1.Size = new System.Drawing.Size(141, 89);
             this.listBox1.TabIndex = 3;
             // 
             // txtRecorrido
             // 
-            this.txtRecorrido.Location = new System.Drawing.Point(9, 41);
+            this.txtRecorrido.Location = new System.Drawing.Point(42, 42);
             this.txtRecorrido.Name = "txtRecorrido";
-            this.txtRecorrido.Size = new System.Drawing.Size(230, 25);
+            this.txtRecorrido.Size = new System.Drawing.Size(194, 25);
             this.txtRecorrido.TabIndex = 4;
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.groupBox1.BackColor = System.Drawing.Color.YellowGreen;
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.btnAnchura);
             this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Controls.Add(this.txtRecorrido);
             this.groupBox1.Controls.Add(this.btnProfundidad);
             this.groupBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 370);
+            this.groupBox1.Location = new System.Drawing.Point(684, 321);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(535, 89);
+            this.groupBox1.Size = new System.Drawing.Size(388, 138);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Recorridos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Recorrer por:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 21);
+            this.label2.Location = new System.Drawing.Point(6, 22);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(230, 17);
             this.label2.TabIndex = 6;
@@ -110,7 +120,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.BackColor = System.Drawing.Color.ForestGreen;
+            this.groupBox2.BackColor = System.Drawing.Color.YellowGreen;
             this.groupBox2.Controls.Add(this.btnHonduras);
             this.groupBox2.Controls.Add(this.btnElSalvador);
             this.groupBox2.Controls.Add(this.btnGuatemala);
@@ -155,32 +165,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(81, 42);
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.Size = new System.Drawing.Size(50, 18);
             this.label1.TabIndex = 7;
             this.label1.Text = "label1";
             // 
-            // groupBox3
+            // label3
             // 
-            this.groupBox3.BackColor = System.Drawing.Color.ForestGreen;
-            this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(831, 352);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(241, 107);
-            this.groupBox3.TabIndex = 8;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Coordenadas";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Coordenadas: Puntero";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.YellowGreen;
-            this.ClientSize = new System.Drawing.Size(1084, 471);
-            this.Controls.Add(this.groupBox3);
+            this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
@@ -190,9 +198,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -208,7 +215,8 @@
         private System.Windows.Forms.Button btnElSalvador;
         private System.Windows.Forms.Button btnGuatemala;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
